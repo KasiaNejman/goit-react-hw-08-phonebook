d7ccc8ad35c92ee8f9780adc2656067d2bd1cf1c
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-03-17 20:54:26 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+            <App />
+          </BrowserRouter>
+        </ChakraProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
